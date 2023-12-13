e56502917fb9f0672e7f451a9a09dbea7888f54f
Suman Mondal <91243593+SM76020@users.noreply.github.com>
2023-12-13 19:22:53 +0530

    Time

---
+++ script.js
@@ -0,0 +1,46 @@
+function startCountdown() {
+  const dateInput = document.getElementById('date').value;
+  const timeInput = document.getElementById('time').value;
+
+  if (!dateInput || !timeInput) {
+    displayMessage('Please select both date and time.');
+    return;
+  }
+
+  const targetDateTimeString = dateInput + 'T' + timeInput;
+  const targetDate = new Date(targetDateTimeString);
+
+  if (isNaN(targetDate.getTime())) {
+    displayMessage('Invalid date or time format.');
+    return;
+  }
+
+  if (targetDate <= new Date()) {
+    displayMessage('Selected time has already passed.');
+    return;
+  }
+
+  function updateTimer() {
+    const timeRemaining = targetDate - new Date();
+    const [days, hours, minutes, seconds] = [
+      Math.floor(timeRemaining / (1000 * 60 * 60 * 24)),
+      Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+      Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60)),
+      Math.floor((timeRemaining % (1000 * 60)) / 1000),
+    ];
+
+    document.getElementById('timer').innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+
+    if (timeRemaining <= 0) {
+      clearInterval(timerInterval);
+      displayMessage('Time Expired!');
+    }
+  }
+
+  updateTimer();
+  const timerInterval = setInterval(updateTimer, 1000);
+}
+
+function displayMessage(message) {
+  document.getElementById('timer').innerHTML = message;
+}
